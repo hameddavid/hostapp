3a7c49acb1d524458bf7afd9e48c7caae25e0261
toyosiAyo <80500110+toyosiAyo@users.noreply.github.com>
2024-01-11 11:30:19 +0100

    Update Payment.php

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -11,7 +11,7 @@ class Payment extends Model
 {
     use HasFactory;
    
-    protected $fillable = ['user_id' ,'product_id', 'amount','part_pay','invoiceReference','transactionReference','url','account_number','payment_date_time'];
+    protected $fillable = ['user_id' ,'product_id', 'amount','part_pay','invoice_number','invoiceReference','transactionReference','url','account_number','payment_date_time'];
 
     public function user(){
         return $this->belongsTo(User::class, 'user_id');
