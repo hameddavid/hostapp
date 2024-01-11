5e55e2328c18fb0bff96b0449408e414c39a9563
toyosiAyo <80500110+toyosiAyo@users.noreply.github.com>
2024-01-11 01:53:27 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PaymentController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/get-transaction-status', [PaymentController::class, 'get_payment_status']);
