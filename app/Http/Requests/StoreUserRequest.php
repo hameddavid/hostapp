35a5a013e875aa204771675e9380df0faa113935
toyosiAyo <80500110+toyosiAyo@users.noreply.github.com>
2024-05-08 19:21:32 +0100

    Update StoreUserRequest.php

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -24,7 +24,7 @@ class StoreUserRequest extends FormRequest
         return [
             'name' => ['required','string', 'max:255'],
             'email' => ['required','email', 'unique:users'],
-            'password' => ['required','string', 'min:8', 'confirmed']
+            'password' => ['required','string', 'min:8']
        
         ];
     }
